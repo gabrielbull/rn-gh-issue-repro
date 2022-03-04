ad0b689e95123b53aee7d50a0499f65e8ad81957
Gabriel Bull <me@gabrielbull.com>
2022-03-04 11:10:10 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+
+const App = () => null;
+
+AppRegistry.registerComponent(appName, () => App);
